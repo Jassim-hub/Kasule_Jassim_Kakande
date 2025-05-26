f9ee5240c14fc4da6e4d43861dc249cac746f9d8
jassim-hub <jassimkasule@gmail.com>
2025-05-26 11:19:01 +0300

    Added abstraction in class work

---
+++ Abstraction.py
@@ -0,0 +1,34 @@
+# abstraction
+# a class that contains one or more methods
+# Abstract class and interfaces
+from abc import ABC, abstractmethod  # Abstraction
+
+
+class Vehicle(ABC):
+    def start(self):
+        print("Vehicle started")
+
+
+class Car(Vehicle):
+    def start(self):
+        print("Car engine started")
+
+
+class Bike(Vehicle):
+    def start(self):
+        print("Bike engine started")
+
+
+# NOTES
+# We cannot create objects of abstract classes
+# vehicle = Vehicle()  # This will raise an error
+car = Car()
+bike = Bike()
+# Call the start method
+car.start()  # Output: Car engine started
+bike.start()  # Output: Bike engine started
+# Call the start method on the abstract class
+Vehicle().start()  # Output: Vehicle started
+# EXERCISE
+# Submit your work on github for method overriding and method resolution order(MRO)
+# Uni system
